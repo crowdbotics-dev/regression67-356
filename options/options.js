da5da8f2b48bfa76c0e6b02d7a429ac1b3a08ab0
ravinder yadav <ravinder.y@crowdbotics.com>
2024-05-13 13:05:15 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "regression67-356",
-  url: "https://regression67-356.botics.co",
-  api: "https://regression67-356.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "regression67-356",
+  "url": "https://regression67-356.botics.co",
+  "api": "https://regression67-356.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
